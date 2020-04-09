aa68b6bca2641c34d3d37bb56b334ff2bf15304a
qiv718 <Arslan.Zubair@CapitalOne.com>
2020-04-09 16:27:37 -0400

    Adding config file with ability to set the crawler type from the application.properties file

--- src/main/java/org/codingexercise/buildit/webcrawler/Initializer.java
+++ src/main/java/org/codingexercise/buildit/webcrawler/Initializer.java
@@ -7,8 +7,12 @@ import org.codingexercise.buildit.webcrawler.crawler.checker.UrlChecker;
 import org.codingexercise.buildit.webcrawler.crawler.checker.ValidUrlChecker;
 import org.codingexercise.buildit.webcrawler.pojo.PageResult;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.time.Duration;
 import java.time.LocalDateTime;
+import java.util.Properties;
 import java.util.Scanner;
 import java.util.function.Consumer;
 
@@ -21,19 +25,31 @@ import java.util.function.Consumer;
  */
 public class Initializer {
     private final static String EXIT_KEYWORD = "EXIT";
+    private static final String CONFIG_FILE = "/Users/qiv718/Desktop/az/web-crawler/src/main/resources/application.properties";
+    private static final String CRAWLER_TYPE_KEY = "crawler.type";
     private final UrlChecker urlValidator = new ValidUrlChecker();
-    private final Crawler crawler = CrawlerFactory.createDepthFirstCrawler();
-//    private final Crawler crawler = CrawlerFactory.createBreadthFirstCrawler();
     private final Consumer<PageResult> resultConsumer = new ConsolePrinter();
+    private Crawler crawler;
 
     public void begin() {
         String userInput = getValidUserEnteredUrl();
         if (!EXIT_KEYWORD.equalsIgnoreCase(userInput)) {
+            crawler = CrawlerFactory.createCrawler(getCrawlerTypeFromConfig(CONFIG_FILE));
             final LocalDateTime start = LocalDateTime.now();
             crawler.crawl(userInput, resultConsumer);
             final LocalDateTime end = LocalDateTime.now();
             System.out.println("--------------------------------------------------");
             System.out.printf("\nTime taken to crawl %s: %s\n", userInput, Duration.between(start, end));
+        }
+    }
+
+    private String getCrawlerTypeFromConfig(final String configFile) {
+        try {
+            final Properties properties = new Properties();
+            properties.load(new FileInputStream(configFile));
+            return properties.getProperty(CRAWLER_TYPE_KEY);
+        } catch (IOException e) {
+            throw new RuntimeException("Config file not found: " + configFile);
         }
     }
 
--- src/main/java/org/codingexercise/buildit/webcrawler/crawler/CrawlerFactory.java
+++ src/main/java/org/codingexercise/buildit/webcrawler/crawler/CrawlerFactory.java
@@ -4,6 +4,21 @@ package org.codingexercise.buildit.webcrawler.crawler;
  * Factory class to help the crawler user to easily select a crawler for their needs.
  */
 public class CrawlerFactory {
+    private static final String CRAWLER_TYPE_DEPTH = "depth";
+    private static final String CRAWLER_TYPE_BREADTH = "breadth";
+
+    /**
+     * Returns a Crawler based on the String parameter.
+     */
+
+    public static Crawler createCrawler(final String crawlerType) {
+        if (CRAWLER_TYPE_DEPTH.equals(crawlerType))
+            return CrawlerFactory.createDepthFirstCrawler();
+        else if (CRAWLER_TYPE_BREADTH.equals(crawlerType))
+            return CrawlerFactory.createBreadthFirstCrawler();
+        else
+            throw new IllegalArgumentException("Unsupported Crawler type: " + crawlerType);
+    }
 
     /**
      * Returns a Crawler that takes a starting URL and iteratively searches across the breadth of the
